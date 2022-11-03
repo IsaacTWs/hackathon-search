801520d80c590b6918e9d8f424e8d1d0b0db3657
wasadmin <wasadmin@wasadmin.com>
2022-11-03 16:03:49 -0500

    Adding logic to data display

--- search-react/src/components/Employee.js
+++ search-react/src/components/Employee.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Employee = ({ onClick, selectedId, employee }) => {
+const Employee = ({ onClick, selectedId, employee, login }) => {
+  if(employee.manager === login.name || login.role === "HR" || employee.email === login.email){
   return (
     <tr className={employee.id === selectedId ? "selected" : ""}
 		    onClick={(e) => onClick(e, employee.id)} >
         <td>{employee.name}</td><td>{employee.email}</td><td>{employee.phone}</td><td>{employee.role}</td><td>{employee.manager}</td><td>{employee.location}</td><td>{employee.salary}</td>
     </tr>
   );
+  }else{
+    return (
+      <tr className={employee.id === selectedId ? "selected" : ""}
+          onClick={(e) => onClick(e, employee.id)} >
+          <td>{employee.name}</td><td>{employee.email}</td><td>{employee.phone}</td><td>{employee.role}</td><td>{employee.manager}</td><td>{employee.location}</td><td></td>
+      </tr>
+    );
+  }
 }
 
 Employee.propTypes = {
