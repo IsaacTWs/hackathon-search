fa9161bcd187eeb19a95179b1fd9b308aa9f1a80
wasadmin <wasadmin@wasadmin.com>
2022-11-03 10:15:55 -0500

    server works yay

--- search-react/src/Main.js
+++ search-react/src/Main.js
@@ -1,40 +1,36 @@
 import React from 'react';
-import CustomerList from './components/CustomerList';
-import CustomerForm from './components/CustomerForm';
-import EventList from './components/EventList';
-import EventForm from './components/EventForm';
-import RegistrationList from './components/RegistrationList';
-import RegistrationForm from './components/RegistrationForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeForm from './components/EmployeeForm';
 import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
 
 import './Main.css';
 
-function CustomersModule() {
+function EmployeesModule() {
   return (
     <div>
-      <CustomerList></CustomerList>
-      <CustomerForm></CustomerForm>
+      <EmployeeList></EmployeeList>
+      <EmployeeForm></EmployeeForm>
     </div>
   );
 }
 
-function EventsModule() {
-  return (
-    <div>
-      <EventList></EventList>
-      <EventForm></EventForm>
-    </div>
-  );
-}
+// function EventsModule() {
+//   return (
+//     <div>
+//       <EventList></EventList>
+//       <EventForm></EventForm>
+//     </div>
+//   );
+// }
 
-function RegistrationsModule() {
-  return (
-    <div>
-      <RegistrationList></RegistrationList>
-      <RegistrationForm></RegistrationForm>
-    </div>
-  );
-}
+// function RegistrationsModule() {
+//   return (
+//     <div>
+//       <RegistrationList></RegistrationList>
+//       <RegistrationForm></RegistrationForm>
+//     </div>
+//   );
+// }
 
 
 function App() {
@@ -43,14 +39,14 @@ function App() {
     <div className="App">
         <h1>Registration App</h1>
         <nav>
-        <Link type='button' className='button btn-outline-primary btn-lg' to="/customers">Customers</Link>
+        <Link type='button' className='button btn-outline-primary btn-lg' to="/employees">Employees</Link>
         <Link type='button' className='button btn-outline-primary btn-lg'  to="/events">Events</Link>
         <Link type='button' className='button btn-outline-primary btn-lg' to="/registrations">Registrations</Link>
         </nav>
-        <Route exact path="/" render={() => (<Redirect to="/customers"/>)}/>
-        <Route path="/customers" component={CustomersModule} />
-        <Route path="/events" component={EventsModule} />
-        <Route path="/registrations" component={RegistrationsModule} />
+        <Route exact path="/" render={() => (<Redirect to="/employees"/>)}/>
+        <Route path="/employees" component={EmployeesModule} />
+        {/* <Route path="/events" component={EventsModule} />
+        <Route path="/registrations" component={RegistrationsModule} /> */}
     </div>
     </Router>
   );
